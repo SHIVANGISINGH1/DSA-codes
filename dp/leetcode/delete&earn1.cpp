cc5e096a5744752f58671ed1cd1f3a1be1d3e829
Shivangi Singh <shivangisinghhappy@gmail.com>
2022-07-26 19:29:54 +0530

    other code added

---
+++ dp/leetcode/delete&earn1.cpp
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<vector>
+#include<unordered_map>
+using namespace std;
+
+class Solution {
+public: 
+    
+    int deleteAndEarn(vector<int>& nums) {
+        
+        unordered_map<int, int> mp;
+        int n = nums.size();
+        
+        for (int idx=0; idx<n; idx++) {
+            
+            if (mp.find(nums[idx]) != mp.end()) {
+                int val = mp[nums[idx]];
+                mp[nums[idx]] = val+1;
+            }
+            else {
+                mp.insert({nums[idx], 1});
+            }
+        }
+        
+        int maxNumber = -1;
+        
+        for (auto it=mp.begin(); it!=mp.end(); it++) {
+            int val = it->first;
+            
+            maxNumber = max(maxNumber,val);
+        }
+        
+        vector<int> dp(maxNumber+1,0);
+        
+        dp[0] = 0;
+        dp[1] = (mp.find(1) != mp.end()) ? mp[1] : 0;
+        
+        for (int idx=2; idx<=maxNumber; idx++) {
+            
+            int gain = (mp.find(idx) != mp.end()) ? mp[idx]*idx : 0;
+            dp[idx] = max(dp[idx-2]+gain, dp[idx-1]);
+        }
+        
+        return dp[maxNumber];
+    }
+};
